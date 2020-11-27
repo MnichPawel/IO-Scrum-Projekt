26f15d7579e85773072c09bce86d5ebac36ec474
MnichPawel <mnichpawel@gmail.com>
2020-11-27 16:06:51 +0100

    bazowy projekt

---
+++ Kalkulator-Serwer/Server.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Kalkulator_Serwer
+{
+    public class Server
+    {
+        public delegate void TransmissionDataDelegate(NetworkStream stream);
+
+        TcpListener listener;
+        //TcpClient client;
+        //NetworkStream stream;
+
+        IPAddress ip_address;
+        int port_number;
+
+        int number_of_connected_clients = 0;
+
+        /// <summary>
+        /// Konstruktor pobierający adres IP oraz port
+        /// </summary>
+        /// <param name="ip"></param>
+        /// <param name="port"></param>
+        public Server(IPAddress ip, int port)
+        {
+            ip_address = ip;
+            port_number = port;
+
+            listener = new TcpListener(ip, port);
+        }
+        /// <summary>
+        /// Funkcja która kończy się dopiero gdy do serwera podepnie się klient
+        /// </summary>
+        public void WaitForClients()
+        {
+            while (true)
+            {
+                listener.Start();
+                TcpClient client = listener.AcceptTcpClient();
+                NetworkStream stream = client.GetStream();
+                TransmissionDataDelegate transmissionDelegate = new TransmissionDataDelegate(ServerLoop);
+
+                transmissionDelegate.BeginInvoke(stream, TransmissionCallback, client);
+            }
+        }
+
+        private void TransmissionCallback(IAsyncResult ar)
+        {
+            TcpClient tcpClient = (TcpClient)ar.AsyncState;
+            tcpClient.Close();
+        }
+
+        /// <summary>
+        /// Funkcja która oczekuje na wiadomości oraz na nie odpowiada, działa aż do przesłania ciągu znaków EXIT
+        /// </summary>
+        public void ServerLoop(NetworkStream stream)
+        {
+            number_of_connected_clients++;
+
+            stream.ReadTimeout = 100000;
+            byte[] buffer_get = new byte[1024];
+            byte[] buffer_send;
+
+            buffer_send = Encoding.UTF8.GetBytes("Wprowadzaj liczby pojedynczo. W odpowiedzi otrzymasz te liczby spierwiastkowane. Napisz EXIT jeśli chcesz wyjść. Napisz CLIENTS jeśli chcesz poznać liczbę klientów aktualnie podłączonych. Zależnie od ustawień komputera jako przecinek stosuje się , lub .\r\n");
+            stream.Write(buffer_send, 0, buffer_send.Length);
+
+            while (true)
+            {
+                try
+                {
+                    int size_recv = stream.Read(buffer_get, 0, 1024);
+
+                    string str = Encoding.UTF8.GetString(buffer_get, 0, size_recv);
+
+                    //Ignorowane są przejścia do następnej lini
+                    if (str.Equals("\r\n")) { continue; }
+
+                    //Zakończ pętle jeśli otrzymano string EXIT
+                    if (str.Equals("EXIT")) { break; }
+
+                    string output_string;
+                    double input_number;
+
+                    //Jeśli konwersja na typ double się uda wyślij wynik, jeśli nie to poinformuj użytkownika
+                    if (Double.TryParse(str, out input_number))
+                    {
+                        double result = Math.Sqrt(input_number);
+
+                        output_string = "sqrt(" + str + ") = " + result.ToString() + "\r\n";
+                    }
+                    else
+                    {
+                        //Zwróć liczbę podłączonych klientów
+                        if (str.Equals("CLIENTS"))
+                        {
+                            output_string = "Liczba klientów: " + number_of_connected_clients.ToString() + "\r\n";
+                        }
+                        else
+                        {
+                            output_string = "Nie wprowadzono liczby.\r\n";
+                        }
+                    }
+
+                    buffer_send = Encoding.UTF8.GetBytes(output_string);
+
+                    stream.Write(buffer_send, 0, buffer_send.Length);
+                }
+                catch (IOException e)
+                {
+                    break;
+                }
+            }
+
+            number_of_connected_clients--;
+        }
+    }
+}
---
+++ Projekt-IO-Kalkulator/Program.cs
@@ -0,0 +1,20 @@
+﻿using Kalkulator_Serwer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Net;
+
+namespace Projekt_IO_Kalkulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server server = new Server(IPAddress.Parse("127.0.0.1"), 2000);
+
+            server.WaitForClients(); //Serwer czeka na klientow
+        }
+    }
+}
